8348695a92f1e850b48b2c9eaf107713a19a58c2
danielbom <daniel.rodrigues.45@hotmail.com>
2021-08-11 14:53:41 -0300

    Add basic task on storeProject()

--- backend/tests/unit/index.spec.js
+++ backend/tests/unit/index.spec.js
@@ -3,6 +3,7 @@
 const { STAGES } = require("../../src/operations/constants");
 const goBackTaskStage = require("../../src/operations/goBackTaskStage");
 const advanceTaskStage = require("../../src/operations/advanceTaskStage");
+const storeProject = require("../../src/operations/storeProject");
 
 describe("Update task stage", () => {
   test("advanceTaskStage must returns 'null' if stage is not defined", () => {
@@ -27,5 +28,38 @@ describe("Update task stage", () => {
     for (let i = 1; i < STAGES.length; i++) {
       expect(typeof goBackTaskStage(STAGES[i])).toBe("string");
     }
+  });
+});
+
+describe("Store project", () => {
+  test("Must store a project if all fields are passed", async () => {
+    const data = {
+      projectName: "Test",
+      clientPersonId: 0,
+      managerPersonId: 1,
+      startDate: new Date(),
+      devIds: [2],
+      getClientByPersonId(x) {
+        return x;
+      },
+      getManagerByPersonId(x) {
+        return x;
+      },
+      storeDevsInDatabase(devs) {},
+      storeProjectInDatabase(project) {
+        return 1;
+      },
+    };
+    const result = await storeProject(data);
+
+    expect(result).toMatchObject({
+      id: 1,
+      start_date: data.startDate,
+      final_date: null,
+      name: data.projectName,
+      client_id: data.clientPersonId,
+      manager_id: data.managerPersonId,
+      devs: [{ project_id: 1, person_id: 2 }],
+    });
   });
 });
