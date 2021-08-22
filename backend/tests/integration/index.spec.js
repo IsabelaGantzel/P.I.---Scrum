52c1149a22430f278ad63ff36bb2756af63a626e
danielbom <daniel.rodrigues.45@hotmail.com>
2021-08-21 22:03:51 -0300

    Test authorization

--- backend/tests/integration/index.spec.js
+++ backend/tests/integration/index.spec.js
@@ -2,15 +2,16 @@
 const { afterAll, describe, expect, test } = require("@jest/globals");
 const app = require("../../src/app");
 const request = require("supertest");
-const config = require("../../src/config");
 const db = require("../../src/database");
+const passwordManager = require("../../src/services/passwordManager");
 
 describe("Api", () => {
   afterAll(() => {
     db.query.destroy();
   });
+
   describe("POST /api/auth/login", () => {
-    test("Must fail if body as incorrect (1)", async () => {
+    test("Must fail if body as incorrect (missing body)", async () => {
       const res = await request(app)
         .post("/api/auth/login")
         .expect("Content-Type", /json/);
@@ -18,7 +19,7 @@ describe("Api", () => {
       expect(res.body).toHaveProperty("error");
       expect(res.body.error).toHaveLength(2);
     });
-    test("Must fail if body as incorrect (2)", async () => {
+    test("Must fail if body as incorrect (missing password)", async () => {
       const res = await request(app)
         .post("/api/auth/login")
         .send({ userName: "admin" })
@@ -27,7 +28,16 @@ describe("Api", () => {
       expect(res.body).toHaveProperty("error");
       expect(res.body.error).toHaveLength(1);
     });
-    test("Must fail if body as invalid (3)", async () => {
+    test("Must fail if body as invalid (invalid userName)", async () => {
+      const res = await request(app)
+        .post("/api/auth/login")
+        .send({ userName: "anyone", password: "invalid-password" })
+        .expect("Content-Type", /json/);
+
+      expect(res.body).toHaveProperty("error");
+    });
+    test("Must fail if body as invalid (invalid password)", async () => {
+      jest.spyOn(passwordManager, "checkPassword").mockResolvedValue(false);
       const res = await request(app)
         .post("/api/auth/login")
         .send({ userName: "admin", password: "invalid-password" })
@@ -36,13 +46,36 @@ describe("Api", () => {
       expect(res.body).toHaveProperty("error");
     });
     test("Must works if body was correct", async () => {
+      jest.spyOn(passwordManager, "checkPassword").mockResolvedValue(true);
       const res = await request(app)
         .post("/api/auth/login")
-        .send({ userName: "admin", password: config.ADMIN_PASSWORD })
+        .send({ userName: "admin", password: "valid-password" })
         .expect("Content-Type", /json/);
 
       expect(res.body).toHaveProperty("message");
       expect(res.body).toHaveProperty("token");
     });
+  });
+
+  let authCount = 0;
+  function authRequired(route, method = "get") {
+    test(`Must return error if not authorized (${authCount++})`, async () => {
+      const res = await request(app)
+        [method](route)
+        .expect("Content-Type", /json/);
+
+      expect(res.body).toHaveProperty("error");
+      expect(res.body.error).toBe("Missing authorization token");
+    });
+  }
+
+  describe("POST /api/projects", () => {
+    authRequired("/api/projects", "post");
+  });
+  describe("GET /api/projects", () => {
+    authRequired("/api/projects");
+  });
+  describe("GET /api/projects/1", () => {
+    authRequired("/api/projects/1");
   });
 });
