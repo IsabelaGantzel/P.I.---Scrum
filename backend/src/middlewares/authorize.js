6d1e64ed27d4fc4f9aa6d6bedb1ac421aca3be45
danielbom <daniel.rodrigues.45@hotmail.com>
2021-08-27 18:11:19 -0300

    Inprove authorize() middleware

--- backend/src/middlewares/authorize.js
+++ backend/src/middlewares/authorize.js
@@ -9,10 +9,17 @@ async function authorize(req, res, next) {
       req.locals.token = await jwtManager.readToken(token);
       next();
     } catch (err) {
-      if (err.name === "TokenExpiredError") {
-        res.status(401).json({ error: "Authorization token expired" });
-      } else {
-        next(err);
+      switch (err.name) {
+        case "TokenExpiredError":
+          res.status(401).json({ error: "Expired authorization token" });
+          break;
+        case "JsonWebTokenError":
+        case "NotBeforeError":
+          res.status(401).json({ error: "Invalid authorization token" });
+          break;
+        default:
+          next(err);
+          break;
       }
     }
   } else {
