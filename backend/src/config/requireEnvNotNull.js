e3f899d8e5ebcee562c01697f1836e728bd12702
danielbom <daniel.rodrigues.45@hotmail.com>
2021-08-27 18:10:56 -0300

    Improve requireEnvNotNull()

--- backend/src/config/requireEnvNotNull.js
+++ backend/src/config/requireEnvNotNull.js
@@ -1,6 +1,10 @@
 function requireEnvNotNull(name) {
   const variable = process.env[name];
-  if (variable === null || variable === undefined) {
+  if (
+    variable === null ||
+    variable === undefined ||
+    (variable.startsWith("[") && variable.endsWith("]"))
+  ) {
     throw new Error(`Environment variable '${name}' can't be null!`);
   } else {
     return variable;
