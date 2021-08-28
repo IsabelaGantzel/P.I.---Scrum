278e516b98cbbf3b2891b6eff4cdf97ad278bdb5
IsabelaGantzel <belagan@msn.com>
2021-08-28 18:14:08 -0300

    Create getTasksWithStage.js

---
+++ backend/script/getTasksWithStage.js
@@ -0,0 +1,13 @@
+const db = require("../src/database");
+
+async function run() {
+  const tasks = await db.query
+    .select("t.*", "sg.name")
+    .from({ t: "tasks" })
+    .join({ st: "sprint_tasks" }, "st.task_id", "=", "t.id")
+    .join({ sg: "stages" }, "sg.id", "=", "st.stage_id");
+
+  console.log(tasks);
+}
+
+run();
